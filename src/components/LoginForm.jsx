3f64ebfabaad7aa41d2329e12f4fae49ab33a98c
levy <levymuguro@gmail.com>
2022-11-01 13:52:21 +0300

    first commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = {'Project-ID': "7103f433-343f-400b-a01e-bbe3e5cff450", 'User-Name': username, 'User-secret': password}
+
+        try {
+        //username / password=> chatengine-> give message
+
+            await axios.get('https://api.chatengine.io/chats', {headers:authObject});
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+
+        //works out -> logged in
+  
+        } catch (error) {
+        // error--> try new username.
+        setError('ooops!! incorrect credentials')
+            
+        }
+
+
+    }
+
+    return(
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Applicatiion</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value = {username} onChange = {(e) => setUsername(e.target.value) } className = "input" placeholder="Username" required />
+                    <input type="password" value = {password} onChange = {(e) => setPassword(e.target.value) } className = "input" placeholder="Password" required />
+                    <div align = "center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+
+                        </button>
+
+                    </div>
+                    <h2 className="error">{error}</h2>
+
+                </form>
+
+            </div>
+
+        </div>
+    );
+}
+export default LoginForm;    
