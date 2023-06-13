7bce07c8ed3beaad3285802886da3a7fd3786664
levy <lnjiru43@gmail.com>
2023-06-13 08:07:09 +0300

    removed module next/server

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-import { userAgent } from "next/server";
 import { useNavigate, Link } from "react-router-dom";
 
 const Signup = () => {
